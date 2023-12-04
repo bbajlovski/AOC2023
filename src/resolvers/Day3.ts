7888ac58b63dbf459c45068e3de9006a99a896ce
bbajlovski <bbajlovski@yahoo.com>
2023-12-04 21:45:26 +0100

    Day 3 / Part 2 - Solved.

--- src/resolvers/Day3.ts
+++ src/resolvers/Day3.ts
@@ -18,6 +18,7 @@ export const resolveOne = async (filename: string): Promise<any> => {
         lines.push(line);   
     });
     await events.once(reader, 'close');
+
     let rowIndex = 0;
     lines.forEach(line => {
         let symbols = line.split("");
@@ -26,15 +27,17 @@ export const resolveOne = async (filename: string): Promise<any> => {
         let numberEndIndex = -1;
         let partNumber = "";
         symbols.forEach((symbol) => {
+
             if (isDigit(symbol)) {
                 if (numberStartIndex === -1) {
                     numberStartIndex = columnIndex;
                 }
                 partNumber += symbol;
             }
+
             if (!isDigit(symbol) && numberStartIndex > -1) {
                 numberEndIndex = columnIndex - 1;
-                const adjacent = isSymbolAdjacent(lines, numberStartIndex, numberEndIndex, rowIndex);
+                const adjacent = hasAdjacentSymbol(lines, numberStartIndex, numberEndIndex, rowIndex);
                 partsSum += adjacent ? +partNumber : 0;
                 numberStartIndex = -1;
                 numberEndIndex = -1;
@@ -43,7 +46,7 @@ export const resolveOne = async (filename: string): Promise<any> => {
 
             if (columnIndex === symbols.length - 1 && numberStartIndex > -1) {
                 numberEndIndex = columnIndex;
-                const adjacent = isSymbolAdjacent(lines, numberStartIndex, numberEndIndex, rowIndex);
+                const adjacent = hasAdjacentSymbol(lines, numberStartIndex, numberEndIndex, rowIndex);
                 partsSum += adjacent ? +partNumber : 0;
                 numberStartIndex = -1;
                 numberEndIndex = -1;
@@ -55,8 +58,6 @@ export const resolveOne = async (filename: string): Promise<any> => {
         rowIndex++;
     });  
 
-    
-
     return "" + partsSum;
 }
 
@@ -67,17 +68,89 @@ export const resolveTwo = async (filename: string): Promise<any> => {
         crlfDelay: Infinity
     });
 1
-    let sum = 0;  
-    reader.on('line', (line) => {
-        sum = 0;
+    let gearSum = 0;  
+    let lines: string[] = [];
+    await reader.on('line', (line) => {
+        lines.push(line);   
     });
-
     await events.once(reader, 'close');
 
-    return "" + sum;
+    let rowIndex = 0;
+    lines.forEach(line => {
+        let symbols = line.split("");
+        let columnIndex = 0;
+        symbols.forEach((symbol) => {
+            let gear = 1;
+            let gearOperands = 0;
+            if (symbol === "*") {
+                let partialLines: string[] = [];
+                // previous line, if exists
+                if (rowIndex > 0) {
+                    partialLines.push(lines[rowIndex - 1]);
+                }
+                // current line
+                partialLines.push(lines[rowIndex]);
+                // next line, if exists
+                if (rowIndex < lines.length - 1) {
+                    partialLines.push(lines[rowIndex + 1]);
+                }                
+
+                let partialRowIndex = 0;
+                partialLines.forEach(partialLine => {
+                    let partialSymbols = partialLine.split("");
+                    let partialColumnIndex = 0;
+
+                    let numberStartIndex = -1;
+                    let numberEndIndex = -1;
+                    let partNumber = "";
+                    partialSymbols.forEach((partialSymbol) => {
+
+                        if (isDigit(partialSymbol)) {
+                            if (numberStartIndex === -1) {
+                                numberStartIndex = partialColumnIndex;
+                            }
+                            partNumber += partialSymbol;
+                        }
+                        
+                        if (!isDigit(partialSymbol) && numberStartIndex > -1) {
+                            numberEndIndex = partialColumnIndex - 1;
+                            const adjacent = columnIndex >= numberStartIndex - 1 && columnIndex <= numberEndIndex + 1;
+                            gear *= adjacent ? +partNumber : 1;
+                            gearOperands += adjacent ? 1 : 0;
+                            numberStartIndex = -1;
+                            numberEndIndex = -1;
+                            partNumber = "";
+                        }
+            
+                        if (partialColumnIndex === partialSymbols.length - 1 && numberStartIndex > -1) {
+                            numberEndIndex = partialColumnIndex;
+                            const adjacent = columnIndex >= numberStartIndex - 1 && columnIndex <= numberEndIndex + 1;
+                            gear *= adjacent ? +partNumber : 1;
+                            gearOperands += adjacent ? 1 : 0;
+                            numberStartIndex = -1;
+                            numberEndIndex = -1;
+                            partNumber = "";
+                        }
+                        partialColumnIndex++;
+                    });
+                    partialRowIndex++;
+                });
+
+
+            }
+
+            columnIndex++;
+
+            gearSum += gearOperands > 1 ? (gear === 1 ? 0 : gear) : 0;
+        });
+
+        rowIndex++;
+    });
+
+    return "" + gearSum;
 }
 
-const isSymbolAdjacent = (lines: string[], numberStartIndex: number, numberEndIndex: number, rowIndex: number) : boolean => {
+const hasAdjacentSymbol = (lines: string[], numberStartIndex: number, numberEndIndex: number, rowIndex: number) : boolean => {
     let result = false;
 
     for (let row = rowIndex - 1; row <= rowIndex + 1; row++) {
